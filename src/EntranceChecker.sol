47d298ddb45401695fc02044bdb5f6601b6d6af6
Filip <82238474+jumperdevweb3@users.noreply.github.com>
2024-10-16 16:21:08 +0200

    forwarder

--- src/EntranceChecker.sol
+++ src/EntranceChecker.sol
@@ -5,8 +5,9 @@ import "forge-std/console.sol";
 import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import "../lib/openzeppelin-contracts/contracts/utils/Strings.sol";
 import "../lib/openzeppelin-contracts/contracts/token/ERC1155/IERC1155.sol";
+import "../lib/openzeppelin-contracts/contracts/metatx/ERC2771Context.sol";
 
-contract EntranceChecker is Ownable {
+contract EntranceChecker is ERC2771Context, Ownable {
     address public ticketAddress;
     bool public isEventFinished;
 
@@ -22,23 +23,26 @@ contract EntranceChecker is Ownable {
 
     event EntryRegistered(address indexed sender, uint256 timestamp, uint256 ticketId);
 
-
-    constructor(address _ticketAddress, address _owner) Ownable(_owner){
+    constructor(address _ticketAddress, address _owner, address _trustedForwarder)
+    ERC2771Context(_trustedForwarder)
+    Ownable(_owner)
+    {
         ticketAddress = _ticketAddress;
     }
 
     function entry(uint256 _ticketId) external {
+        address sender = _msgSender();
         require(!isEventFinished, "Event has finished");
-        require(!hasEntered[msg.sender], "Address has already entered");
+        require(!hasEntered[sender], "Address has already entered");
         require(!usedTickets[_ticketId], "This ticket has been already used");
 
         IERC1155 nftContract = IERC1155(ticketAddress);
-        require(nftContract.balanceOf(msg.sender, _ticketId) > 0, "Caller does not own this ticket");
+        require(nftContract.balanceOf(sender, _ticketId) > 0, "Caller does not own this ticket");
 
-        entries.push(Entry(msg.sender, block.timestamp, _ticketId));
-        hasEntered[msg.sender] = true;
+        entries.push(Entry(sender, block.timestamp, _ticketId));
+        hasEntered[sender] = true;
         usedTickets[_ticketId] = true;
-        emit EntryRegistered(msg.sender, block.timestamp, _ticketId);
+        emit EntryRegistered(sender, block.timestamp, _ticketId);
     }
 
     function hasEntry(address _address) external view returns (bool) {
@@ -55,5 +59,18 @@ contract EntranceChecker is Ownable {
 
     function getTicketAddress() external view returns (address) {
         return ticketAddress;
+    }
+
+    // Override functions to resolve conflicts
+    function _msgSender() internal view override(Context, ERC2771Context) returns (address) {
+        return ERC2771Context._msgSender();
+    }
+
+    function _msgData() internal view override(Context, ERC2771Context) returns (bytes calldata) {
+        return ERC2771Context._msgData();
+    }
+
+    function _contextSuffixLength() internal view override(Context, ERC2771Context) returns (uint256) {
+        return ERC2771Context._contextSuffixLength();
     }
 }
