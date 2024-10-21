c1a9d832fca30b5d96c6c4de4c32484c2b71ee19
adiusz <leeszczuu@gmail.com>
2024-10-21 17:43:42 +0200

    adjust for Paymaster, ditch EIP-2771

--- src/EntranceChecker.sol
+++ src/EntranceChecker.sol
@@ -7,9 +7,12 @@ import "../lib/openzeppelin-contracts/contracts/utils/Strings.sol";
 import "../lib/openzeppelin-contracts/contracts/token/ERC1155/IERC1155.sol";
 import "../lib/openzeppelin-contracts/contracts/metatx/ERC2771Context.sol";
 
-contract EntranceChecker is ERC2771Context, Ownable {
+contract EntranceChecker is Ownable {
+    address public ownerSmartWallet;
     address public ticketAddress;
     bool public isEventFinished;
+    mapping(address => bool) private hasEntered;
+    mapping(uint256 => bool) private usedTickets;
 
     struct Entry {
         address wallet;
@@ -18,16 +21,24 @@ contract EntranceChecker is ERC2771Context, Ownable {
     }
 
     Entry[] public entries;
-    mapping(address => bool) private hasEntered;
-    mapping(uint256 => bool) private usedTickets;
 
-    event EntryRegistered(address indexed sender, uint256 timestamp, uint256 ticketId);
+    event Entrance  (address indexed sender, uint256 timestamp, uint256 ticketId);
 
-    constructor(address _ticketAddress, address _owner, address _trustedForwarder)
-    ERC2771Context(_trustedForwarder)
-    Ownable(_owner)
-    {
+    constructor(
+        address _owner,
+        address _ownerSmartWallet,
+        address _ticketAddress
+    ) Ownable(_owner) {
         ticketAddress = _ticketAddress;
+        ownerSmartWallet = _ownerSmartWallet;
+    }
+
+    function _checkOwner() internal view override {
+        require(owner() == _msgSender() || ownerSmartWallet == _msgSender(), "Not owner");
+    }
+
+    function setSmartWallet(address _smartWallet) external onlyOwner {
+        ownerSmartWallet = _smartWallet;
     }
 
     function entry(uint256 _ticketId) external {
@@ -42,7 +53,7 @@ contract EntranceChecker is ERC2771Context, Ownable {
         entries.push(Entry(sender, block.timestamp, _ticketId));
         hasEntered[sender] = true;
         usedTickets[_ticketId] = true;
-        emit EntryRegistered(sender, block.timestamp, _ticketId);
+        emit Entrance   (sender, block.timestamp, _ticketId);
     }
 
     function hasEntry(address _address) external view returns (bool) {
@@ -59,18 +70,5 @@ contract EntranceChecker is ERC2771Context, Ownable {
 
     function getTicketAddress() external view returns (address) {
         return ticketAddress;
-    }
-
-    // Override functions to resolve conflicts
-    function _msgSender() internal view override(Context, ERC2771Context) returns (address) {
-        return ERC2771Context._msgSender();
-    }
-
-    function _msgData() internal view override(Context, ERC2771Context) returns (bytes calldata) {
-        return ERC2771Context._msgData();
-    }
-
-    function _contextSuffixLength() internal view override(Context, ERC2771Context) returns (uint256) {
-        return ERC2771Context._contextSuffixLength();
     }
 }
